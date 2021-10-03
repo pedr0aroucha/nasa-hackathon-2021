200b8678c86d74a133dcca62ad4d640c8e4086c8
pedr0aroucha <contato.pedr0aroucha@gmail.com>
2021-10-03 02:06:16 -0300

    releasing the colors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const { resolve } = require("path");
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use(express.static(resolve(__dirname)));
 
